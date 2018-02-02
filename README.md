61ba0b6549654a1bcb1628599d3c2b0e429044a0
yuanzhoucq <yuanzhou.cq@gmail.com>
2018-02-03 00:29:44 +0100

    - version 0.2

---
+++ README.md
@@ -0,0 +1,7 @@
+A console application of speaking made with Baidu Yuyin.
+
+## Dependency
+Console application `mpg123`.
+
+## Usage
+`$ ilang "The text you want to read."`
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ilangbd",
+    version='0.2',
+    description="A console application of speaking made with Baidu Yuyin",
+    long_description="speak",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=True,
+    author="joyuer",
+    author_email="yuanzhou.cq@gmail.com",
+    url="https://github.com/yuanzhoucq/ilangbd",
+    license='MIT',
+    install_requires=[
+        'Click',
+        'requests',
+    ],
+    entry_points='''
+        [console_scripts]
+        ilang=ilangbd.main:read
+    ''',
+)
